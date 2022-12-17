7b4e2ea4f022635d9272fb5ff1281960b337b697
Filip Rosa <filip.rosa2@gmail.com>
2022-12-17 22:02:15 +0100

    Prepínanie na druhú stránku

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 from tkinter import *
 
-def ShowNameDataBtn():
-    user_name = ent.get()
-    
-    print(user_name)
-
 root_frame = Tk()
 root_frame.geometry("1000x600")
 root_frame.title("VLAKOVÝ DISPEČING")
 root_frame.config(background="black")
-
 icon = PhotoImage(file="icon.gif")
 root_frame.iconphoto(True, icon)
 
-main_label = Label(root_frame, text="Vitajte na pracovisku vlakového dispečingu pre trať Prievidza - Chynorany", fg="white", bg="black", font=("Century Schoolbook", 17))
-main_label.place(x = 120, y = 200)
+def FirstPage():
+    def SecondPage():
+        main_label.destroy()
+        btn.destroy()
 
-name_label = Label(root_frame, text="Zadajte Vaše meno: ", fg="white", bg="black")
-name_label.place(x = 330, y = 300)
+        main_label_2 = Label(root_frame, text="Prievidza - Chynorany", fg="white", bg="black", font=("Century Schoolbook", 17))
+        main_label_2.place(x = 120, y = 200)
 
-ent = Entry(root_frame)
-ent.place(x = 450, y = 300)
+    main_label = Label(root_frame, text="Vitajte na pracovisku vlakového dispečingu pre trať Prievidza - Chynorany", fg="white", bg="black", font=("Century Schoolbook", 17))
+    main_label.place(x = 120, y = 200)
 
-btn = Button(root_frame, text="ENTER", command=ShowNameDataBtn)
-btn.place(x = 580, y = 297)
+    btn = Button(root_frame, text="ZAČAŤ HRAŤ", command=SecondPage)
+    btn.place(x = 450, y = 300)
 
+FirstPage()
 root_frame.mainloop()
