90b4a7c4918b2a60d304a471f8ce71c8f090d902
Filip Rosa <filip.rosa2@gmail.com>
2022-12-17 19:04:29 +0100

    Vytvorenie okna cez funkciu

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-print("Ahoj")
-print("Ahoj2")
+from tkinter import *
+from tkinter import ttk
+
+
+def CreateWindow():
+    root_frame = Tk()
+    root_frame.title("VLAKOVÝ DISPEČING")
+    root_frame.geometry("800x500")
+
+    icon = PhotoImage(file="icon.gif")
+    root_frame.iconphoto(True, icon)
+
+    root_frame.mainloop()
+
+
+CreateWindow()
