4fad6d7b3d5207b4546f4a49066177cd06b0e94e
Filip Rosa <filip.rosa2@gmail.com>
2022-12-19 23:07:15 +0100

    Práca na tlačítku -> zadanie user_name.

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ def OpenScheme():
     return scheme_img.show()
 
 def FirstPage():
+    def FirstPageWithName():
+        main_label.destroy()
+        user_name.destroy
+
+        btn = Button(root_frame, text="POĎME NA TO!", command=SecondPage)
+        btn.place(x = 450, y = 300)
+
+
     def SecondPage():
         main_label.destroy()
         btn.destroy()
@@ -28,8 +36,14 @@ def FirstPage():
     main_label = Label(root_frame, text="Vitajte na pracovisku vlakového dispečingu pre trať Prievidza - Chynorany", fg="white", bg="black", font=("Century Schoolbook", 17))
     main_label.place(x = 120, y = 200)
 
-    btn = Button(root_frame, text="POĎME NA TO!", command=SecondPage)
-    btn.place(x = 450, y = 300)
+    user_label = Label(root_frame, text="Zadajte svoje meno:", fg="white", bg="black", font=("Century Schoolbook", 17))
+    user_label.place(x = 200, y = 300)
+
+    user_name = Entry(root_frame)
+    user_name.place(x = 420, y = 310)
+
+    btn_user_name = Button(root_frame, text="ENTER", command=FirstPageWithName)
+    btn_user_name.place(x = 550, y = 305)
 
 FirstPage()
 root_frame.mainloop()
